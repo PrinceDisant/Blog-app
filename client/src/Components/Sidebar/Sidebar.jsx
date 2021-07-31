f12bda7a12f7c3308d8c7b030a108852c1a9dfb2
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-07-31 20:51:08 -0230

    Sidebar and Posts

---
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <p>Self taught Full Stack Developer</p>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import Header from '../../Components/Header/Header'
-import './home.css';
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Posts from '../../Components/Posts/Posts'
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./home.css";
 
 function Home() {
-    return (
-        <div className="home">
-            <Header />
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <div className="home">
+          <Posts />
+          <Sidebar />
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
