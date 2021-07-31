16c548f2ddc239d0b768366646e60b121ae67ebc
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-07-31 19:35:12 -0230

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      New blog app
+      <Navbar />
     </div>
   );
 }
---
+++ client/src/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="nav">
+            <div className="left">
+                <i className="navIcon fab fa-facebook-f"></i>
+                <i className="navIcon fab fa-twitter"></i>
+                <i className="navIcon fab fa-instagram"></i>
+            </div>
+            <div className="center">
+                <ul className="navList">
+                    <li className="navListItem">HOME</li>
+                    <li className="navListItem">ABOUT</li>
+                    <li className="navListItem">CONTACT</li>
+                    <li className="navListItem">WRITE</li>
+                    <li className="navListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="right">
+                <img 
+                    className="displayPicture"
+                    src="https://images.pexels.com/photos/6437888/pexels-photo-6437888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+                    alt="profile pic"
+                />
+                <i className="navSearchIcon fas fa-search"></i>    
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
