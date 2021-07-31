4f06442f57a4c4fbf5807e2e879f4181dc656a36
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-07-31 20:21:41 -0230

    Header Chnages

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 
 function Header() {
-    return (
-        <div className="header">
-             
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">LEARN TO</span>
+        <span className="headerTitleLg">CODE</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/4050299/pexels-photo-4050299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
+  );
 }
 
-export default Header
+export default Header;
