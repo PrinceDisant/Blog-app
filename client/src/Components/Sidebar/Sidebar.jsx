54758b6a25219e33514a20db995945315c8fa324
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-08-01 09:14:45 -0230

    p

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -7,6 +7,7 @@ function Sidebar() {
             <div className="sidebarItem">
                 <span className="sidebarTitle">ABOUT ME</span>
                 <p>Self taught Full Stack Developer</p>
+                <p>The Beginning of 2020 I found myself in a situation where I had to learn web development very fast, but I did not have the know-hows of finding the right resources or navigating through them them. Which is precicely why I will structure this website to help you in your journey of learning to code</p>
             </div>
         </div>
     )
