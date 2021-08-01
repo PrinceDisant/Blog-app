7732a5dff7f4f4a4bc59d32891aa32c825949ece
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-08-01 10:02:52 -0230

    sidebar

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import './sidebar.css';
+import React from "react";
+import "./sidebar.css";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebarItem">
-                <span className="sidebarTitle">ABOUT ME</span>
-                <p>Self taught Full Stack Developer</p>
-                <p>The Beginning of 2020 I found myself in a situation where I had to learn web development very fast, but I did not have the know-hows of finding the right resources or navigating through them them. Which is precicely why I will structure this website to help you in your journey of learning to code</p>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <p>Self-taught Full Stack Developer</p>
+        <p>
+          At the Beginning of 2020, I found myself in a situation where I had to
+          learn web development very fast, but I did not have the know-how of
+          finding the right resources or navigating through them. This is
+          precisely why I will structure this website to help YOU in your
+          journey of becoming a coder.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATAGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Front-End</li>
+          <li className="sidebarListItem">Back-End</li>
+          <li className="sidebarListItem">Health</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-f"></i>
+          <i className="sidebarIcon fab fa-twitter"></i>
+          <i className="sidebarIcon fab fa-instagram"></i>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
