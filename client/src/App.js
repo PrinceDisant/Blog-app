3f348c749d6242f07ee56a4acf756a01ebad82dd
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-08-02 19:07:45 -0230

    write

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 // import Home from './pages/home/Home';
-import Single from './pages/single/Single';
+// import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Single />
+      <Write />
     </div>
   );
 }
---
+++ client/src/pages/write/Write.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './write.css';
+
+function Write() {
+    return (
+        <div className="write">
+            <img className="writeImg" src="https://images.pexels.com/photos/4385547/pexels-photo-4385547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput"><i className="writeIcon fas fa-plus"></i></label>
+                    <input type="file" id="fileInput" style={{display: "none"}}/>
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="One, Two... Write!" type="text" className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
+        </div>
+    )
+}
+
+export default Write
