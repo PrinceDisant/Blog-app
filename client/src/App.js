0dad07a90c94b3e8dda705bba08be8ad3c21d345
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-08-03 14:34:14 -0230

    remove link from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Settings from './pages/settings/Settings';
 import Home from './pages/home/Home';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   const user = false;
