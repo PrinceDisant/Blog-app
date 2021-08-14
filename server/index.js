4cdd63504aee39cdf381f8f4152fa4bd0406fdbb
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-08-14 18:12:42 -0230

    multer

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const catagoryRoute = require("./routes/catagories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -19,10 +20,24 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-  app.use("/server/auth", authRoute);
-  app.use("/server/users", userRoute);
-  app.use("/server/posts", postRoute);
-  app.use("/server/catagories", catagoryRoute);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({storage:storage});
+app.post("/server/upload", upload.single("file"),(req,res)=>{
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/server/auth", authRoute);
+app.use("/server/users", userRoute);
+app.use("/server/posts", postRoute);
+app.use("/server/catagories", catagoryRoute);
 app.listen("5000", () => {
   console.log("Backend is running...");
 });
