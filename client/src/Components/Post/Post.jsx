1728e367d85f4241d01dfe4b94a72062a75abccf
Disant Upadhyay <79222290+PrinceDisant@users.noreply.github.com>
2021-08-22 21:41:59 -0230

    post, singlePost integration

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Post.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Post({ post }) {
+  const PF = "http://localhost:5000/images/";
   return (
     <div className="post">
-      {post.photo && <img className="postImg" src={post.photo} alt="" />}
+      <Link to={`/post/${post._id}`} className="link">
+        {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      </Link>
       <div className="postInfo">
         <div className="postCatagories">
           {post.categories.map((c) => (
@@ -13,7 +16,7 @@ function Post({ post }) {
           ))}
         </div>
         <Link to={`/post/${post._id}`} className="link">
-            <span className="postTitle">{post.title}</span>
+          <span className="postTitle">{post.title}</span>
         </Link>
         <hr />
         <span className="postDate">
